47e29b884bd8833c9264040d8a28a5e3a34e5056
SASSE2705 <107636982+SasserolliJuan@users.noreply.github.com>
2023-12-18 01:33:26 -0300

    Delete Ejercicios Generales de Programación/General/General - Librerías/Listas/Listas Enlazadas directory

--- Ejercicios Generales de Programación/General/General - Librerías/Listas/Listas Enlazadas/Listas Enlazadas Simples/main.c
+++
@@ -1,9 +0,0 @@
-#include "st_lista_simple.h"
-
-int main()
-{
-    nodo_lista_simple * aux = inic_lista_simple();
-    aux = cargar_lista_simple(aux);
-    mostrar_lista_simple(aux);
-    return 0;
-}
